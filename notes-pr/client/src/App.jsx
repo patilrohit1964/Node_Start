3d8346935239f774d368408e0de5b9b0e4b1786d
rohitinfikey <rohit.patil@infikeytechnologies.com>
2024-12-19 16:54:29 +0530

    added note backend

--- notes-pr/client/src/App.jsx
+++ notes-pr/client/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 import Home from './pages/Home'
 import Footer from './pages/Footer'
 import Navbar from './components/Navbar'
+import { Button } from "./components/ui/button"
+import { HStack } from "@chakra-ui/react"
 
 
 const routers = createBrowserRouter([
@@ -17,9 +19,54 @@ function App() {
     <div>
       <Navbar />
       <RouterProvider router={routers} />
+      <Demo />
       <Footer />
     </div>
   )
 }
 
 export default App
+
+
+import { Button } from "@/components/ui/button"
+import {
+  DialogActionTrigger,
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+const Demo = () => {
+  return (
+    <DialogRoot>
+      <DialogTrigger asChild>
+        <Button variant="outline" size="sm">
+          Open Dialog
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Dialog Title</DialogTitle>
+        </DialogHeader>
+        <DialogBody>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </DialogBody>
+        <DialogFooter>
+          <DialogActionTrigger asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogActionTrigger>
+          <Button>Save</Button>
+        </DialogFooter>
+        <DialogCloseTrigger />
+      </DialogContent>
+    </DialogRoot>
+  )
+}
--- notes-pr/client/src/main.jsx
+++ notes-pr/client/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import { ToastContainer } from 'react-toastify'
-import { BrowserRouter } from 'react-router-dom'
-
+import App from './App.jsx'
+import './index.css'
+import { Provider } from "./components/ui/provider.jsx"
 createRoot(document.getElementById('root')).render(
   <>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
     <ToastContainer position='top-right' autoClose={2000} hideProgressBar={true} closeOnClick={true} pauseOnHover={true} draggable={true} />
   </>,
 )
