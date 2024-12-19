284e7d4fbe5ecf0310b01ea1e75d915875c6d72b
rohitinfikey <rohit.patil@infikeytechnologies.com>
2024-12-19 16:25:57 +0530

    added note backend

--- notes-pr/client/src/main.jsx
+++ notes-pr/client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ToastContainer } from 'react-toastify'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <App />
-  </StrictMode>,
+    <ToastContainer position='top-right' autoClose={2000} hideProgressBar={true} closeOnClick={true} pauseOnHover={true} draggable={true} />
+  </>,
 )
