e428ce37c4d0423d88a7aa235425b23207c36374
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-22 23:36:14 +0530

    added text color

--- notes-pr/client/src/pages/NoteEdit.jsx
+++ notes-pr/client/src/pages/NoteEdit.jsx
@@ -107,7 +107,7 @@ const NoteEdit = ({ onClose, noteData }) => {
                             type="text"
                             value={title}
                             onChange={(e) => setTitle(e.target.value)}
-                            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-purple-500 bg-white"
+                            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-purple-500 bg-white text-black"
                         />
                     </div>
 
@@ -120,7 +120,7 @@ const NoteEdit = ({ onClose, noteData }) => {
                             type="text"
                             value={description}
                             onChange={(e) => setDescription(e.target.value)}
-                            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-purple-500 bg-white"
+                            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-purple-500 bg-white text-black"
                         />
                     </div>
 
